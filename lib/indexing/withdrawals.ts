3d6b1a6b89094bc0f75a10af5a026e264aa26c82
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2023-04-18 23:41:32 -0300

    Add withdrawals indexing functions

---
+++ lib/indexing/withdrawals.ts
@@ -0,0 +1,46 @@
+import { provider } from "../provider";
+import { supabase } from "../supabaseClient";
+import { chunkMap } from "./utils/chunk";
+import { STRATEGY_MANAGER_ADDRESS } from "./utils/constants";
+import { StrategyManager__factory } from "../../typechain";
+
+interface Withdrawal {
+  block: number;
+  // (depositor, nonce) should be replaced by queued withdrawal ID
+  depositor: string;
+  nonce: bigint;
+}
+
+async function indexWithdrawalsRange(startingBlock: number, currentBlock: number, chunkSize: number) {
+  const index: Withdrawal[] = [];
+
+  const strategyManager = StrategyManager__factory.connect(STRATEGY_MANAGER_ADDRESS, provider);
+  
+  await Promise.all(
+    chunkMap(startingBlock, currentBlock, chunkSize, async (fromBlock, toBlock) => {
+      const withdrawalLogs = await strategyManager.queryFilter(strategyManager.getEvent("WithdrawalCompleted"), fromBlock, toBlock);
+
+      withdrawalLogs.forEach(log => {
+        index.push({
+          block: log.blockNumber,
+          depositor: log.args.depositor,
+          nonce: log.args.nonce,
+        });
+      });
+    })
+  );
+
+  return index;
+}
+
+export async function indexWithdrawals() {
+  const lastRow = await supabase
+    .from("_Withdrawals")
+    .select("block")
+    .order("block", { ascending: false })
+    .limit(1);
+  const startingBlock = (lastRow.data !== null)? lastRow.data[0].block : 0;
+  const currentBlock = await provider.getBlockNumber();
+
+  return await indexWithdrawalsRange(startingBlock, currentBlock, 10_000);
+}
