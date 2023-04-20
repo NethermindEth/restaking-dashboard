f0c1d02d8d42b7c5e676ed53ea3a29f6fd09ad1a
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2023-04-20 01:29:36 -0300

    Use free RPC in rates functions

--- lib/indexing/utils/rates.ts
+++ lib/indexing/utils/rates.ts
@@ -1,8 +1,9 @@
 import { IStrategy__factory, RocketTokenRETH__factory, StEth__factory } from "@/typechain";
 import { RETH_ADDRESS, STETH_ADDRESS, STETH_STRATEGY_ADDRESS } from "./constants";
-import { provider } from "../../provider";
+import { ethers } from "ethers";
 
 export async function stEthSharesExchangeRate(): Promise<bigint> {
+  const provider = new ethers.JsonRpcProvider("https://rpc.ankr.com/eth_goerli");
   const strategy = IStrategy__factory.connect(STETH_STRATEGY_ADDRESS, provider);
   const stEth = StEth__factory.connect(STETH_ADDRESS, provider);
 
@@ -10,6 +11,7 @@ export async function stEthSharesExchangeRate(): Promise<bigint> {
 }
 
 export async function rEthSharesExchangeRate(): Promise<bigint> {
+  const provider = new ethers.JsonRpcProvider("https://rpc.ankr.com/eth_goerli");
   const rEth = RocketTokenRETH__factory.connect(RETH_ADDRESS, provider);
 
   return await rEth.getExchangeRate();
