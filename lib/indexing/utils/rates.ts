3cb3ddb39f61647c1e77cea379de85cf6a032e0b
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2023-04-25 15:31:24 -0300

    Add global client provider

--- lib/indexing/utils/rates.ts
+++ lib/indexing/utils/rates.ts
@@ -1,16 +1,19 @@
+import { clientProvider } from "@/lib/clientProvider";
 import { IStrategy__factory, RocketTokenRETH__factory } from "../../../typechain";
 import { RETH_ADDRESS, STETH_STRATEGY_ADDRESS } from "./constants";
 import { ethers } from "ethers";
 
-export async function stEthSharesExchangeRate(): Promise<bigint> {
-  const provider = new ethers.JsonRpcProvider("https://rpc.ankr.com/eth_goerli");
+export async function stEthSharesExchangeRate(
+  provider: ethers.Provider = clientProvider,
+): Promise<bigint> {
   const strategy = IStrategy__factory.connect(STETH_STRATEGY_ADDRESS, provider);
 
   return await strategy.sharesToUnderlyingView(BigInt(1e18));
 }
 
-export async function rEthSharesExchangeRate(): Promise<bigint> {
-  const provider = new ethers.JsonRpcProvider("https://rpc.ankr.com/eth_goerli");
+export async function rEthSharesExchangeRate(
+  provider: ethers.Provider = clientProvider,
+): Promise<bigint> {
   const rEth = RocketTokenRETH__factory.connect(RETH_ADDRESS, provider);
 
   return await rEth.getExchangeRate();
