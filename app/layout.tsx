e4a03676357f997d9f1e4e9dcda21b4ddac93800
Edgar Barrantes <edgar@barrantes.dev>
2023-04-25 21:33:29 -0600

    Adds generated image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,39 @@
 import "./globals.css";
 
+const title = "EigenLayer Restaking Dashboard";
+const description = "Visualise EigenLayer staking and withdrawals in Goerli";
+const url =
+  `https://${process.env.VERCEL_URL}/api/og` ||
+  "https://restaking-dashboard-git-twitter-card-restaking-dashboard.vercel.app/api/og";
+
 export const metadata = {
-  title: "EigenLayer Restaking Dashboard",
-  description: "Visualise EigenLayer staking and withdrawals in Goerli",
+  title,
+  description,
+  openGraph: {
+    type: "website",
+    title,
+    description,
+    images: [
+      {
+        url,
+        secureUrl: url,
+        href: url,
+        alt: title,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    description,
+    images: [
+      {
+        url,
+        secureUrl: url,
+        href: url,
+        alt: title,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
---
+++ pages/api/og.tsx
@@ -0,0 +1,217 @@
+import { supabase } from "@/lib/supabaseClient";
+import {
+  BlockData,
+  mergeBlockChunks,
+  roundToDecimalPlaces,
+  sumTotalAmounts,
+} from "@/lib/utils";
+import { ImageResponse } from "@vercel/og";
+
+export const config = {
+  runtime: "edge",
+};
+
+const logo = fetch(new URL("../../public/logo.png", import.meta.url)).then(
+  (res) => res.arrayBuffer()
+);
+
+const stEthLogo = fetch(
+  new URL("../../public/steth_logo.png", import.meta.url)
+).then((res) => res.arrayBuffer());
+
+const rEthLogo = fetch(new URL("../../public/reth.png", import.meta.url)).then(
+  (res) => res.arrayBuffer()
+);
+
+const beaconEthLogo = fetch(
+  new URL("../../public/beaconChainETH.png", import.meta.url)
+).then((res) => res.arrayBuffer());
+
+// const font = fetch(
+//   new URL("../../public/ABCReproVariable.ttf", import.meta.url)
+// ).then((res) => res.arrayBuffer());
+
+export default async function () {
+  const {
+    totalrEthDeposits,
+    totalstEthDeposits,
+    totalstEthWithdrawals,
+    totalrEthWithdrawals,
+    totalBeaconChainStakes,
+  } = await getDeposits();
+  const [logoData, stEthLogoData, rEthLogoData, beaconEthLogoData] =
+    await Promise.all([logo, stEthLogo, rEthLogo, beaconEthLogo]);
+  // const logoData = await logo;
+  // const stEthLogoData = await stEthLogo;
+  // const rEthLogoData = await rEthLogo;
+  // const beaconEthLogoData = await beaconEthLogo;
+  // const fontData = await font;
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: "flex",
+          fontWeight: "bold",
+          height: "100%",
+          width: "100%",
+          flexDirection: "column",
+          textAlign: "center",
+          justifyContent: "center",
+          alignItems: "center",
+          background:
+            "linear-gradient(to bottom, rgb(255, 255, 255), rgb(100, 156, 240))",
+          fontFamily: '"Abcreprovariable"',
+        }}
+      >
+        <div
+          style={{ display: "flex" }}
+          tw="z-10 max-w-5xl items-center justify-between font-mono text-sm"
+        >
+          <div
+            style={{ display: "flex" }}
+            tw="items-center justify-center static h-auto w-auto bg-none lgmb-12"
+          >
+            <img
+              alt="EigenLayer Logo"
+              width="64"
+              height="72"
+              src={logoData as unknown as string}
+            />
+            <p tw="text-2xl ml-4">EigenLayer Stats on Testnet</p>
+          </div>
+        </div>
+        <div
+          style={{ display: "flex" }}
+          tw="my-8 w-full lg:w-1/2 flex flex-wrap flex-col lg:flex-row lg:flex-nowrap items-stretch justify-center"
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              color: "rgba(235, 235, 235, 1)",
+              backgroundColor: "rgb(26, 12, 109)",
+              textAlign: "center",
+            }}
+            tw="grow mt-0 py-8 px-24 mx-4 shadow-lg rounded-md text-center"
+          >
+            <img
+              tw="mx-auto"
+              src={stEthLogoData as unknown as string}
+              alt="stETH"
+              width="48"
+              height="48"
+            />
+            <p tw="text-sm md:text-base">Staked stETH</p>
+            <p tw="md:text-xl">
+              {roundToDecimalPlaces(totalstEthDeposits - totalstEthWithdrawals)}
+            </p>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              color: "rgb(26, 12, 109)",
+              backgroundColor: "rgb(255, 184, 0)",
+              textAlign: "center",
+            }}
+            tw="grow mt-0 py-8 px-24 mx-4 shadow-lg rounded-md text-center"
+          >
+            <img
+              tw="mx-auto"
+              src={rEthLogoData as unknown as string}
+              alt="rETH"
+              width="48"
+              height="48"
+            />
+            <p tw="text-sm md:text-base">Staked rETH</p>
+            <p tw="md:text-xl">
+              {roundToDecimalPlaces(totalrEthDeposits - totalrEthWithdrawals)}
+            </p>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              color: "rgb(26, 12, 109)",
+              backgroundColor: "rgb(254, 156, 147)",
+              textAlign: "center",
+            }}
+            tw="grow mt-0 py-8 px-24 mx-4 shadow-lg rounded-md text-center"
+          >
+            <img
+              tw="mx-auto"
+              src={beaconEthLogoData as unknown as string}
+              alt="ETH"
+              width="48"
+              height="48"
+            />
+            <p tw="text-base mx-auto">Beacon Chain ETH</p>
+            <p tw="text-xl mx-auto">
+              {roundToDecimalPlaces(totalBeaconChainStakes)}
+            </p>
+          </div>
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 600,
+      // fonts: [
+      //   {
+      //     name: "Abcreprovariable",
+      //     data: fontData,
+      //     style: "normal",
+      //   },
+      // ],
+    }
+  );
+}
+
+async function getDeposits() {
+  // Deposits
+  let { data: rEthDeposits, error: rEthDepositError } = await supabase
+    .from("consumabledailydepositsreth")
+    .select("*");
+  rEthDeposits = mergeBlockChunks(rEthDeposits as BlockData[]);
+  let totalrEthDeposits = sumTotalAmounts(rEthDeposits as BlockData[]);
+
+  let { data: stEthDeposits, error: stEthDepositError } = await supabase
+    .from("consumabledailydepositssteth")
+    .select("*");
+  stEthDeposits = mergeBlockChunks(stEthDeposits as BlockData[]);
+  let totalstEthDeposits = sumTotalAmounts(stEthDeposits as BlockData[]);
+
+  let { data: beaconChainStakes } = await supabase
+    .from("consumablebeaconchainstakeseth")
+    .select("*");
+  beaconChainStakes = mergeBlockChunks(beaconChainStakes as BlockData[]);
+  let totalBeaconChainStakes = sumTotalAmounts(
+    beaconChainStakes as BlockData[]
+  );
+
+  // Withdrawals
+  let { data: rEthWithdrawals, error: rEthWithDrawalsError } = await supabase
+    .from("consumabledailywithdrawalsreth")
+    .select("*");
+  rEthWithdrawals = mergeBlockChunks(rEthWithdrawals as BlockData[]);
+  let totalrEthWithdrawals = sumTotalAmounts(rEthWithdrawals as BlockData[]);
+
+  let { data: stEthWithdrawals, error: stEthWithDrawalsError } = await supabase
+    .from("consumabledailywithdrawalssteth")
+    .select("*");
+  stEthWithdrawals = mergeBlockChunks(stEthWithdrawals as BlockData[]);
+  let totalstEthWithdrawals = sumTotalAmounts(stEthWithdrawals as BlockData[]);
+
+  return {
+    rEthDeposits,
+    totalrEthDeposits,
+    stEthDeposits,
+    totalstEthDeposits,
+    stEthWithdrawals,
+    totalstEthWithdrawals,
+    totalrEthWithdrawals,
+    beaconChainStakes,
+    totalBeaconChainStakes,
+  };
+}
