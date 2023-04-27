651e0db3b7e82591ba2ea14443c1e5b62a005b91
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2023-04-27 17:05:19 -0300

    Add pod stakes indexing edge function

---
+++ supabase/functions/index_pod_stakes/index.ts
@@ -0,0 +1,24 @@
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+
+import { ethers } from "ethers";
+import { createClient } from "@supabase/supabase-js";
+
+import { indexPodStakes } from "../utils/indexing/podStakes.ts";
+
+serve(async () => {
+  try {
+    await indexPodStakes(
+      createClient(
+        Deno.env.get("SUPABASE_URL") ?? "",
+        Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? "",
+      ),
+      new ethers.JsonRpcProvider(Deno.env.get("RPC_URL") ?? "", "goerli"),
+    );
+  
+    return new Response("Indexing successful!\n", { status: 200 });
+  }
+  catch (err) {
+    console.error(err);
+    return new Response("Got an error while indexing, check the logs\n", { status: 500 });
+  }
+});
