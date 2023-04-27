f0ba8b9988c23bf5309c89a4e867d81eb71057b8
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2023-04-26 22:17:17 -0300

    Add documentation to validator indexing

--- lib/indexing/validators.ts
+++ lib/indexing/validators.ts
@@ -12,6 +12,17 @@ interface Validator {
   withdrawalAddress: string;
 }
 
+/**
+ * Simply indexes validators, without any special procedure since in this case
+ * the starting block and simultaneous runs don't really matter as all the
+ * validator balances and withdrawal addresses need to be fully re-indexed
+ * each time.
+ * The data is fetched using the Beacon API and it's stored in the Validators
+ * table in Supabase. Only validators with an L1 address as withdrawal
+ * credentials are added to the database, as the goal of this database is
+ * getting EigenPod-related stakes, and in this case the pod L1 address should
+ * be used as the validator withdrawal credentials.
+ */
 export async function indexValidators() {
   const validators: Validator[] = (
     await getAllValidators(1200, 20)
