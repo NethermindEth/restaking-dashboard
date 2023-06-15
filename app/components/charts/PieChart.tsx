69461fd9949ae040d06a4d21ee039a459154d9ed
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2023-06-14 23:08:56 -0300

    Add cbETH color to PieChart component

--- app/components/charts/PieChart.tsx
+++ app/components/charts/PieChart.tsx
@@ -10,11 +10,13 @@ const dataConfig = {
       backgroundColor: [
         "rgba(26, 12, 109, 1)",
         "rgba(255, 184, 0, 1)",
+        "rgba(0, 153, 153, 1)",
         "rgba(254, 156, 147, 1)",
       ],
       hoverBackgroundColor: [
         "rgba(26, 12, 109, 0.6)",
         "rgba(255, 184, 0, 0.6)",
+        "rgba(0, 153, 153, 1)",
         "rgba(254, 156, 147, 0.6)",
       ],
     },
