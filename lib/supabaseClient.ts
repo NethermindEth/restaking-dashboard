31bfe20dd5d829c7734f54520660a2f8bed240ff
Yasmine Bachi <gy_bachi@esi.dz>
2023-06-27 16:56:01 +0100

    revert supabase client changes

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://wsoqtgziaduigrrhnjkk.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indzb3F0Z3ppYWR1aWdycmhuamtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE3NjE3ODIsImV4cCI6MTk5NzMzNzc4Mn0.b0EdvPtA_9EzeP1mgzfsGekk6X0DWpaWsDjJl0LoaxA";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || "";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
