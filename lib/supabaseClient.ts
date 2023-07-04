22903b9ce333fc993043f7e29398f5da8be43889
Yasmine Bachi <gy_bachi@esi.dz>
2023-07-04 15:45:50 +0100

    setup supabase config

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,14 +1,29 @@
 import { PostgrestError, createClient } from "@supabase/supabase-js";
 import { Database } from "./database.types";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_NEW_SUPABASE_URL || "";
-const supabaseKey = process.env.NEXT_PUBLIC_NEW_SUPABASE_KEY || "";
+const supabaseConfig = {
+  mainnet: {
+    supabaseUrl: process.env.NEXT_PUBLIC_NEW_SUPABASE_URL || "",
+    supabaseKey: process.env.NEXT_PUBLIC_NEW_SUPABASE_KEY || "",
+  },
+  goerli: {
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_KEY || "",
+  },
+};
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
-  db: { schema: "mainnet" },
-});
+export const supabase = createClient<Database>(
+  supabaseConfig.mainnet.supabaseUrl,
+  supabaseConfig.mainnet.supabaseKey,
+  {
+    db: { schema: "mainnet" },
+  }
+);
 
-export function supabaseUnwrap<T>(resp: { data: T, error: PostgrestError | null }) {
+export function supabaseUnwrap<T>(resp: {
+  data: T;
+  error: PostgrestError | null;
+}) {
   if (resp.error) throw resp.error;
 
   return resp.data;
