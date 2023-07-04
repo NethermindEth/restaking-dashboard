f12f9ee0ba49d460bee810791e46811e5a9e9799
Yasmine Bachi <gy_bachi@esi.dz>
2023-07-04 15:54:45 +0100

    moved toggle btn to top right of the page

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -24,17 +24,6 @@ export default function Dashboard(data: any) {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-24 font-semibold">
-      <div className="z-10 max-w-5xl items-center justify-between font-mono text-sm">
-        <div className="h-48 flex w-full items-center justify-center lg:static lg:h-auto lg:w-auto lg:bg-none lgmb-12">
-          <Image
-            src={"/logo.png"}
-            alt="EigenLayer Logo"
-            width={64}
-            height={72}
-          />
-          <p className="text-lg md:text-2xl ml-4">EigenLayer Stats</p>
-        </div>
-      </div>
       <div className="flex self-end items-center">
         <span className="mr-2">Mainnet</span>
         <label className="flex items-center cursor-pointer">
@@ -59,6 +48,18 @@ export default function Dashboard(data: any) {
         </label>
         <span className="ml-2">Testnet</span>
       </div>
+      <div className="z-10 max-w-5xl items-center justify-between font-mono text-sm">
+        <div className="h-48 flex w-full items-center justify-center lg:static lg:h-auto lg:w-auto lg:bg-none lgmb-12">
+          <Image
+            src={"/logo.png"}
+            alt="EigenLayer Logo"
+            width={64}
+            height={72}
+          />
+          <p className="text-lg md:text-2xl ml-4">EigenLayer Stats</p>
+        </div>
+      </div>
+
       <div className="my-8 w-full lg:w-1/2 flex flex-wrap flex-col lg:flex-row lg:flex-nowrap items-stretch justify-center">
         <div className="data-card data-card-steth grow mt-8 lg:mt-0 py-8 px-10 md:px-24 mx-4 shadow-lg rounded-md text-center">
           <span className="inline-block">
