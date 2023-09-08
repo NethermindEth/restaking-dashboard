7eb818203f024f31fe4e8751d86bf370795d41eb
Sameer Kumar <hellosam1010@gmail.com>
2023-09-08 15:10:21 +0530

    added date series to the daily deposit queries

--- lib/spice-queries.ts
+++ lib/spice-queries.ts
@@ -46,27 +46,44 @@ export class SpiceEigenlayerQueries {
     public async dailyBeaconChainETHDeposit() {
         try{
             const result = this.spiceClient.query(`
-            SELECT 
-                GREATEST(
-                    TO_DATE(ep.block_timestamp),
-                    TO_DATE(1606845623 + 32 * 12 * activation_eligibility_epoch),
-                    TO_DATE(COALESCE(bte.block_timestamp, 0))
-                ) as "date",
-                SUM(effective_balance)/POW(10,9) as Daily_Added_Effective_Balance,
-            FROM 
-                eth.beacon.validators vl
-            JOIN 
-                eth.eigenlayer.eigenpods ep
-            ON 
-                vl.withdrawal_credentials = ep.withdrawal_credential
-            LEFT JOIN
-                eth.beacon.bls_to_execution_changes bte
-            ON
-                bte.validator_index = vl.validator_index
-            GROUP BY 
-                "date"
-            ORDER BY 
-                "date" ASC;
+            WITH 
+                Daily_Deposits AS (
+                    SELECT 
+                        GREATEST(
+                            TO_DATE(ep.block_timestamp),
+                            TO_DATE(1606845623 + 32 * 12 * activation_eligibility_epoch),
+                            TO_DATE(COALESCE(bte.block_timestamp, 0))
+                        ) as "date",
+                        SUM(effective_balance)/POW(10,9) as Daily_Added_Effective_Balance
+                    FROM 
+                        eth.beacon.validators vl
+                    JOIN 
+                        eth.eigenlayer.eigenpods ep
+                    ON 
+                        vl.withdrawal_credentials = ep.withdrawal_credential
+                    LEFT JOIN
+                        eth.beacon.bls_to_execution_changes bte
+                    ON
+                        bte.validator_index = vl.validator_index
+                    GROUP BY 
+                        "date"
+                ),
+                Date_Series AS (
+                    SELECT DISTINCT DATE_ADD((SELECT MIN("date") FROM Daily_Deposits), number) AS "date"
+                    FROM eth.blocks
+                    WHERE number <= DATEDIFF(CURRENT_DATE, (SELECT MIN("date") FROM Daily_Deposits))
+                )
+                SELECT 
+                    Date_Series."date",
+                    Daily_Deposits.Daily_Added_Effective_Balance
+                FROM 
+                    Date_Series
+                LEFT JOIN
+                    Daily_Deposits
+                ON 
+                    Date_Series."date" = Daily_Deposits."date"
+                ORDER BY 
+                    "date";
             `)
             return result
         } catch(err: any) {
@@ -78,28 +95,45 @@ export class SpiceEigenlayerQueries {
     public async dailyCumulativeBeaconChainETHDeposit() {
         try{
             const result = this.spiceClient.query(`
-            SELECT 
-                GREATEST(
-                    TO_DATE(ep.block_timestamp),
-                    TO_DATE(1606845623 + 32 * 12 * activation_eligibility_epoch),
-                    TO_DATE(COALESCE(bte.block_timestamp, 0))
-                ) as "date",
-                SUM(effective_balance)/POW(10,9) as Daily_Added_Effective_Balance,
-                SUM(SUM(effective_balance)/POW(10,9)) OVER (ORDER BY "date" ASC) as Cumulative_Daily_Added_Effective_Balance
-            FROM 
-                eth.beacon.validators vl
-            JOIN 
-                eth.eigenlayer.eigenpods ep
-            ON 
-                vl.withdrawal_credentials = ep.withdrawal_credential
-            LEFT JOIN
-                eth.beacon.bls_to_execution_changes bte
-            ON
-                bte.validator_index = vl.validator_index
-            GROUP BY 
-                "date"
-            ORDER BY 
-                "date" ASC;
+            WITH 
+                Daily_Deposits AS (
+                    SELECT 
+                        GREATEST(
+                            TO_DATE(ep.block_timestamp),
+                            TO_DATE(1606845623 + 32 * 12 * activation_eligibility_epoch),
+                            TO_DATE(COALESCE(bte.block_timestamp, 0))
+                        ) as "date",
+                        SUM(effective_balance)/POW(10,9) as Daily_Added_Effective_Balance,
+                        SUM(SUM(effective_balance)/POW(10,9)) OVER (ORDER BY "date" ASC) as Cumulative_Daily_Added_Effective_Balance
+                    FROM 
+                        eth.beacon.validators vl
+                    JOIN 
+                        eth.eigenlayer.eigenpods ep
+                    ON 
+                        vl.withdrawal_credentials = ep.withdrawal_credential
+                    LEFT JOIN
+                        eth.beacon.bls_to_execution_changes bte
+                    ON
+                        bte.validator_index = vl.validator_index
+                    GROUP BY 
+                        "date"
+                ),
+                Date_Series AS (
+                    SELECT DISTINCT DATE_ADD((SELECT MIN("date") FROM Daily_Deposits), number) AS "date"
+                    FROM eth.blocks
+                    WHERE number <= DATEDIFF(CURRENT_DATE, (SELECT MIN("date") FROM Daily_Deposits))
+                )
+                SELECT 
+                    Date_Series."date",
+                    Daily_Deposits.Daily_Added_Effective_Balance
+                FROM 
+                    Date_Series
+                LEFT JOIN
+                    Daily_Deposits
+                ON 
+                    Date_Series."date" = Daily_Deposits."date"
+                ORDER BY 
+                    "date";
             `        
             )
             return result
