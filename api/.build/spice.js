eb1c800c0a5614f7ca54d307def53df0b9d1a8bf
rachit2501 <rac.sri25@gmail.com>
2023-10-03 02:55:32 +0530

    chore: clean unnessesary files

--- api/.build/spice.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var spice_1 = require("@spiceai/spice");
-var dotenv = require("dotenv");
-dotenv.config();
-var spiceClient = new spice_1.SpiceClient(process.env.SPICE || "");
-exports.default = spiceClient;
-//# sourceMappingURL=spice.js.map
