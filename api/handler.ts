d2d70e673f513635d4fa69cb2a2a81be9c93f8d4
Franco Barpp Gomes <franco@nethermind.io>
2023-10-20 00:29:12 -0300

    refactor: Make query block indentations consistent & small query style fixes

--- api/handler.ts
+++ api/handler.ts
@@ -23,94 +23,94 @@ export const getDeposits = async (
 
   const response = (
     await spiceClient.query(`
-  WITH NonCoalescedDailyTokenDeposits AS (
-    SELECT
-        TO_DATE(block_timestamp) AS "date",
-        token,
-        SUM(token_amount) / POWER(10, 18) AS total_amount,
-        SUM(shares) / POWER(10, 18) AS total_shares
-    FROM ${chain}.eigenlayer.strategy_manager_deposits
-    WHERE token IN (${tokenAddressList.map((addr) => `'${addr}'`).join(",")})
-    GROUP BY "date", token
-  ),
-  NonCoalescedDailyValidatorDeposits AS (
+      WITH NonCoalescedDailyTokenDeposits AS (
+        SELECT
+            TO_DATE(block_timestamp) AS "date",
+            token,
+            SUM(token_amount) / POWER(10, 18) AS total_amount,
+            SUM(shares) / POWER(10, 18) AS total_shares
+        FROM ${chain}.eigenlayer.strategy_manager_deposits
+        WHERE token IN (${tokenAddressList.map((addr) => `'${addr}'`).join(",")})
+        GROUP BY "date", token
+      ),
+      NonCoalescedDailyValidatorDeposits AS (
+          SELECT
+              TO_DATE(GREATEST(
+                  COALESCE(ep.block_timestamp, 0),
+                  1606824023 + 32 * 12 * activation_eligibility_epoch,
+                  COALESCE(bte.block_timestamp, 0)
+              )) as "date",
+              NULL as token,
+              count(*) * 32 AS total_amount,
+              count(*) * 32 AS total_shares
+          FROM ${chain}.beacon.validators vl
+          INNER JOIN ${chain}.eigenlayer.eigenpods ep
+              ON vl.withdrawal_credentials = ep.withdrawal_credential
+          LEFT JOIN ${chain}.beacon.bls_to_execution_changes bte
+              ON bte.validator_index = vl.validator_index
+          GROUP BY "date"
+      ),
+      NonCoalescedDailyDeposits AS (
+          SELECT * FROM NonCoalescedDailyTokenDeposits UNION ALL SELECT * FROM NonCoalescedDailyValidatorDeposits
+      ),
+      MinDate AS (
+          SELECT MIN("date") AS min_date FROM NonCoalescedDailyDeposits
+      ),
+      Series AS (
+          SELECT ROW_NUMBER() OVER () as number FROM ${chain}.recent_transactions
+      ),
+      DateSeries AS (
+          SELECT DATE_ADD((SELECT min_date FROM MinDate), number) AS "date"
+          FROM Series
+          WHERE number <= DATEDIFF(CURRENT_DATE, (SELECT min_date FROM MinDate))
+      ),
+      TokenSeries AS (
+          SELECT NULL AS token
+          ${tokenAddressList.map((addr) => `UNION ALL SELECT '${addr}'`).join("\n")}
+      ),
+      TokenDateCoalesceSeries AS (
+          SELECT
+              ds."date",
+              ts.token
+          FROM DateSeries ds
+          CROSS JOIN TokenSeries ts
+      ),
+      CoalescedDailyDeposits AS (
+          SELECT
+              tdcs."date",
+              tdcs.token,
+              COALESCE(ncdd.total_amount, 0) AS total_amount,
+              COALESCE(ncdd.total_shares, 0) AS total_shares
+          FROM TokenDateCoalesceSeries tdcs
+          LEFT JOIN NonCoalescedDailyDeposits ncdd
+              ON tdcs."date" = ncdd."date" AND (tdcs.token = ncdd.token OR (tdcs.token IS NULL AND ncdd.token IS NULL))
+      ),
+      CumulativeDeposits AS (
+          SELECT
+              cdd."date",
+              cdd.token,
+              cdd.total_amount,
+              cdd.total_shares,
+              SUM(COALESCE(cdd.total_amount, 0)) OVER (PARTITION BY cdd.token ORDER BY cdd."date") AS cumulative_amount,
+              SUM(COALESCE(cdd.total_shares, 0)) OVER (PARTITION BY cdd.token ORDER BY cdd."date") AS cumulative_shares
+          FROM CoalescedDailyDeposits cdd
+      ),
+      Last30DaysData AS (
+        SELECT * FROM CumulativeDeposits as ldd
+        WHERE ldd."date" >= DATE_ADD(CURRENT_DATE, -30)
+      )
       SELECT
-          TO_DATE(GREATEST(
-              COALESCE(ep.block_timestamp, 0),
-              1606824023 + 32 * 12 * activation_eligibility_epoch,
-              COALESCE(bte.block_timestamp, 0)
-          )) as "date",
-          NULL as token,
-          count(*) * 32 AS total_amount,
-          count(*) * 32 AS total_shares
-      FROM ${chain}.beacon.validators vl
-      INNER JOIN ${chain}.eigenlayer.eigenpods ep
-          ON vl.withdrawal_credentials = ep.withdrawal_credential
-      LEFT JOIN ${chain}.beacon.bls_to_execution_changes bte
-          ON bte.validator_index = vl.validator_index
-      GROUP BY "date"
-  ),
-  NonCoalescedDailyDeposits AS (
-      SELECT * FROM NonCoalescedDailyTokenDeposits UNION ALL SELECT * FROM NonCoalescedDailyValidatorDeposits
-  ),
-  MinDate AS (
-      SELECT MIN("date") AS min_date FROM NonCoalescedDailyDeposits
-  ),
-  Series AS (
-      SELECT ROW_NUMBER() OVER () as number FROM ${chain}.recent_transactions
-  ),
-  DateSeries AS (
-      SELECT DATE_ADD((SELECT min_date FROM MinDate), number) AS "date"
-      FROM Series
-      WHERE number <= DATEDIFF(CURRENT_DATE, (SELECT min_date FROM MinDate))
-  ),
-  TokenSeries AS (
-      SELECT NULL AS token
-      ${tokenAddressList.map((addr) => `UNION ALL SELECT '${addr}'`).join("\n")}
-  ),
-  TokenDateCoalesceSeries AS (
-      SELECT
-          ds."date",
-          ts.token
-      FROM DateSeries ds
-      CROSS JOIN TokenSeries ts
-  ),
-  CoalescedDailyDeposits AS (
-      SELECT
-          tdcs."date",
-          tdcs.token,
-          COALESCE(ncdd.total_amount, 0) AS total_amount,
-          COALESCE(ncdd.total_shares, 0) AS total_shares
-      FROM TokenDateCoalesceSeries tdcs
-      LEFT JOIN NonCoalescedDailyDeposits ncdd
-          ON tdcs."date" = ncdd."date" AND (tdcs.token = ncdd.token OR (tdcs.token IS NULL AND ncdd.token IS NULL))
-  ),
-  CumulativeDeposits AS (
-      SELECT
-          cdd."date",
-          cdd.token,
-          cdd.total_amount,
-          cdd.total_shares,
-          SUM(COALESCE(cdd.total_amount, 0)) OVER (PARTITION BY cdd.token ORDER BY cdd."date") AS cumulative_amount,
-          SUM(COALESCE(cdd.total_shares, 0)) OVER (PARTITION BY cdd.token ORDER BY cdd."date") AS cumulative_shares
-      FROM CoalescedDailyDeposits cdd
-  ),
-  Last30DaysData AS (
-    SELECT * FROM CumulativeDeposits as ldd
-    WHERE ldd."date" >= DATE_ADD(CURRENT_DATE, -30)
-  )
-  SELECT
-      ldd."date",
-      ldd.token,
-      ldd.total_amount,
-      ldd.total_shares,
-      ldd.cumulative_amount,
-      ldd.cumulative_shares
-  FROM Last30DaysData ldd
-  ORDER BY
-      ldd."date",
-      ldd.token;
-  `)
+          ldd."date",
+          ldd.token,
+          ldd.total_amount,
+          ldd.total_shares,
+          ldd.cumulative_amount,
+          ldd.cumulative_shares
+      FROM Last30DaysData ldd
+      ORDER BY
+          ldd."date",
+          ldd.token;
+    `)
   ).toArray();
 
   const groupedResponse = response.reduce((acc, el) => {
@@ -207,23 +207,23 @@ export const getWithdrawals = async (
 
   const response = (
     await spiceClient.query(`
-  WITH DailyTokenWithdrawals AS (
-    SELECT
-        TO_DATE(block_timestamp) AS "date",
-        token,
-        SUM(token_amount) / POWER(10, 18) AS total_amount,
-        SUM(shares) / POWER(10, 18) AS total_shares
-    FROM ${chain}.eigenlayer.strategy_manager_withdrawal_completed
-    WHERE token IN (${tokenAddressList.map((addr) => `'${addr}'`).join(",")})
-    AND receive_as_tokens
-    GROUP BY "date", token
-   ),
-  DailyValidatorWithdrawals AS (
-    SELECT
-        TO_DATE(1606824023 + 32 * 12 * exit_epoch) as "date",
-        NULL as token,
-        count(*) * 32 AS total_amount,
-        count(*) * 32 AS total_shares
+      WITH DailyTokenWithdrawals AS (
+        SELECT
+            TO_DATE(block_timestamp) AS "date",
+            token,
+            SUM(token_amount) / POWER(10, 18) AS total_amount,
+            SUM(shares) / POWER(10, 18) AS total_shares
+        FROM ${chain}.eigenlayer.strategy_manager_withdrawal_completed
+        WHERE token IN (${tokenAddressList.map((addr) => `'${addr}'`).join(",")})
+        AND receive_as_tokens
+        GROUP BY "date", token
+      ),
+      DailyValidatorWithdrawals AS (
+        SELECT
+            TO_DATE(1606824023 + 32 * 12 * exit_epoch) as "date",
+            NULL as token,
+            count(*) * 32 AS total_amount,
+            count(*) * 32 AS total_shares
         FROM
             ${chain}.beacon.validators vl
         INNER JOIN
@@ -231,65 +231,65 @@ export const getWithdrawals = async (
         ON
             LEFT(vl.withdrawal_credentials, 4) = '0x01' AND vl.withdrawal_credentials = ep.withdrawal_credential
         GROUP BY "date"
-    ),
-  DailyWithdrawals AS (
-      SELECT * FROM DailyTokenWithdrawals UNION ALL SELECT * FROM DailyValidatorWithdrawals
-    ),
-  MinDate AS (
-      SELECT MIN("date") AS min_date FROM DailyTokenWithdrawals
-    ),
-  DateSeries AS (
-      SELECT DISTINCT DATE_ADD((SELECT min_date FROM MinDate), number) AS "date"
-      FROM ${chain}.blocks
-      WHERE number <= DATEDIFF(CURRENT_DATE, (SELECT min_date FROM MinDate))
-    ),
-  TokenSeries AS (
-      SELECT NULL AS token
-      ${tokenAddressList.map((addr) => `UNION ALL SELECT '${addr}'`).join("\n")}
-  ),
-  AllCombinations AS (
-    SELECT 
-        ds."date", 
-        ts.token
-    FROM 
-        DateSeries ds
-    CROSS JOIN 
-        TokenSeries ts
-    ),
-  CumulativeWithdrawals AS (
+      ),
+      DailyWithdrawals AS (
+          SELECT * FROM DailyTokenWithdrawals UNION ALL SELECT * FROM DailyValidatorWithdrawals
+      ),
+      MinDate AS (
+          SELECT MIN("date") AS min_date FROM DailyTokenWithdrawals
+      ),
+      DateSeries AS (
+          SELECT DISTINCT DATE_ADD((SELECT min_date FROM MinDate), number) AS "date"
+          FROM ${chain}.blocks
+          WHERE number <= DATEDIFF(CURRENT_DATE, (SELECT min_date FROM MinDate))
+      ),
+      TokenSeries AS (
+          SELECT NULL AS token
+          ${tokenAddressList.map((addr) => `UNION ALL SELECT '${addr}'`).join("\n")}
+      ),
+      AllCombinations AS (
+        SELECT 
+            ds."date", 
+            ts.token
+        FROM 
+            DateSeries ds
+        CROSS JOIN 
+            TokenSeries ts
+      ),
+      CumulativeWithdrawals AS (
+          SELECT
+              ac."date",
+              ac.token,
+              COALESCE(td.total_amount, 0) AS total_amount,
+              COALESCE(td.total_shares, 0) AS total_shares,
+              SUM(COALESCE(td.total_amount, 0)) OVER (PARTITION BY ac.token ORDER BY ac."date") AS cumulative_amount,
+              SUM(COALESCE(td.total_shares, 0)) OVER (PARTITION BY ac.token ORDER BY ac."date") AS cumulative_shares
+          FROM
+              AllCombinations ac
+          LEFT JOIN
+            DailyWithdrawals td
+          ON
+              ac."date" = td."date"
+          AND
+              (ac.token = td.token OR (ac.token IS NULL AND td.token IS NULL))
+      ),
+      Last30DaysData AS (
+          SELECT * FROM CumulativeWithdrawals as cd
+          WHERE cd."date" >= DATE_ADD(CURRENT_DATE, -30)
+      )
       SELECT
-          ac."date",
-          ac.token,
-          COALESCE(td.total_amount, 0) AS total_amount,
-          COALESCE(td.total_shares, 0) AS total_shares,
-          SUM(COALESCE(td.total_amount, 0)) OVER (PARTITION BY ac.token ORDER BY ac."date") AS cumulative_amount,
-          SUM(COALESCE(td.total_shares, 0)) OVER (PARTITION BY ac.token ORDER BY ac."date") AS cumulative_shares
+        ldd."date",
+        ldd.token,
+        ldd.total_amount,
+        ldd.total_shares,
+        ldd.cumulative_amount,
+        ldd.cumulative_shares
       FROM
-          AllCombinations ac
-      LEFT JOIN
-        DailyWithdrawals td
-      ON
-          ac."date" = td."date"
-      AND
-          (ac.token = td.token OR (ac.token IS NULL AND td.token IS NULL))
-    ),
-    Last30DaysData AS (
-      SELECT * FROM CumulativeWithdrawals as cd
-      WHERE cd."date" >= DATE_ADD(CURRENT_DATE, -30)
-    )
-    SELECT
-      ldd."date",
-      ldd.token,
-      ldd.total_amount,
-      ldd.total_shares,
-      ldd.cumulative_amount,
-      ldd.cumulative_shares
-    FROM
-      Last30DaysData ldd
-    ORDER BY
-      ldd."date",
-      ldd.token;
-  `)
+        Last30DaysData ldd
+      ORDER BY
+        ldd."date",
+        ldd.token;
+    `)
   ).toArray();
 
   const groupedResponse = response.reduce((acc, el) => {
@@ -326,11 +326,13 @@ export async function totalStakedBeaconChainEth(
 
   try {
     const result = await spiceClient.query(`
-          SELECT SUM(effective_balance)/POW(10,9) as final_balance
-          FROM ${chain}.beacon.validators
-          JOIN ${chain}.eigenlayer.eigenpods
-          ON ${chain}.beacon.validators.withdrawal_credentials = ${chain}.eigenlayer.eigenpods.withdrawal_credential AND effective_balance!='0'
-      `);
+      SELECT SUM(effective_balance) / POW(10,9) as final_balance
+      FROM ${chain}.beacon.validators
+      JOIN ${chain}.eigenlayer.eigenpods
+      ON
+        ${chain}.beacon.validators.withdrawal_credentials = ${chain}.eigenlayer.eigenpods.withdrawal_credential
+        AND effective_balance != '0';
+    `);
     return { statusCode: 200, body: result.toString() };
   } catch (err: any) {
     console.log(err);
@@ -353,19 +355,19 @@ export async function stakersBeaconChainEth(
 
   try {
     const result = await spiceClient.query(`
-    SELECT 
-        ${chain}.eigenlayer.eigenpods.pod_owner,
-        SUM(${chain}.beacon.validators.effective_balance) / POWER(10,9) AS total_effective_balance
-    FROM 
-        ${chain}.beacon.validators
-    JOIN 
-        ${chain}.eigenlayer.eigenpods
-    ON 
-        ${chain}.beacon.validators.withdrawal_credentials = ${chain}.eigenlayer.eigenpods.withdrawal_credential 
-    AND ${chain}.beacon.validators.effective_balance != '0'
-    GROUP BY 
-        ${chain}.eigenlayer.eigenpods.pod_owner
-    ORDER BY total_effective_balance DESC;
+      SELECT 
+          ${chain}.eigenlayer.eigenpods.pod_owner,
+          SUM(${chain}.beacon.validators.effective_balance) / POWER(10,9) AS total_effective_balance
+      FROM 
+          ${chain}.beacon.validators
+      JOIN 
+          ${chain}.eigenlayer.eigenpods
+      ON 
+          ${chain}.beacon.validators.withdrawal_credentials = ${chain}.eigenlayer.eigenpods.withdrawal_credential 
+          AND ${chain}.beacon.validators.effective_balance != '0'
+      GROUP BY 
+          ${chain}.eigenlayer.eigenpods.pod_owner
+      ORDER BY total_effective_balance DESC;
     `);
 
     return {
