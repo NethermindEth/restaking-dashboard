7fce04d0b2eb7ed417456718645a9ccf7af9d13d
Franco Barpp Gomes <franco@nethermind.io>
2023-10-20 00:40:24 -0300

    refactor: Remove unused types from api utils

--- api/utils.ts
+++ api/utils.ts
@@ -1,21 +1,3 @@
-export interface DailyTokenData {
-  date: string;
-  total_amount: number;
-  total_shares: number;
-  cumulative_amount: number;
-  cumulative_shares: number;
-}
-
-export interface DailyTokenWithdrawals {
-  date: string | null;
-  total_amount: number | null;
-  total_shares: number | null;
-}
-
-export interface LeaderboardUserData {
-  depositor: string;
-  totalStaked: number;
-}
 
 export const supportedChains = ["eth", "goerli"] as const;
 export type Chain = (typeof supportedChains)[number];
