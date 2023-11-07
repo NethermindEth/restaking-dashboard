01060c7da4ac74d11f742e888f5b6e008e71c099
Franco Barpp Gomes <franco@nethermind.io>
2023-11-07 15:49:37 -0300

    refactor: Refactor Leaderboard component logic

--- app/components/Leaderboard.tsx
+++ app/components/Leaderboard.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChevronLeft,
@@ -9,7 +10,7 @@ import {
 
 import { LeaderboardStaker, useLeaderboard } from "@/app/components/hooks/useLeaderboard";
 import { getEtherscanAddressUrl, getShortenedAddress} from "@/app/utils/address";
-import { SupportedNetwork } from "@/app/utils/types";
+import { SupportedNetwork, SupportedToken } from "@/app/utils/types";
 import { getNetworkTokens, getTokenInfo } from "@/app/utils/constants";
 
 interface LeaderboardProps {
@@ -19,71 +20,54 @@ interface LeaderboardProps {
 export default function Leaderboard({ network }: LeaderboardProps) {
   const { data: leaderboardData } = useLeaderboard(network);
 
+  const PAGE_SIZE = 10;
+
+  const [activeTab, setActiveTab] = useState<"total" | SupportedToken>("total");
   const [activeData, setActiveData] = useState<LeaderboardStaker[]>([]);
-  const [activeButton, setActiveButton] = useState(0);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const tokens = getNetworkTokens(network);
 
   useEffect(() => {
     if (!leaderboardData) return;
 
-    setActiveData(leaderboardData.total);
-    setActiveButton(0);
-    setCurrentPage(1);
+    setActiveTab("total");
   }, [network, leaderboardData]);
 
+  useEffect(() => {
+    if (!leaderboardData) return;
+
+    if (activeTab === "total") {
+      setActiveData(leaderboardData.total);
+    }
+    else {
+      setActiveData(leaderboardData.partial[activeTab] || []);
+    }
+    
+    setTotalPages(Math.ceil(activeData.length / PAGE_SIZE));
+    setCurrentPage(0);
+  }, [activeTab, leaderboardData]);
+
   if (!leaderboardData) return <></>;
-
-  const tokens = getNetworkTokens(network);
-  const tokenLabels = tokens.map(token => getTokenInfo(token).label);
-
-  const tabKeys = ["totals", ...tokenLabels].map(el => el.toLowerCase());
-
-  const PAGE_SIZE = 10;
-  const totalPages = Math.ceil(activeData.length / PAGE_SIZE);
-
-  const handleToggleContent = (data: LeaderboardStaker[], index: number) => {
-    setActiveData(data);
-    setActiveButton(index);
-  };
-
-  const getActiveTabName = (activeButton: number) => {
-    return (tabKeys[activeButton] || "").toLowerCase();
-  };
 
   return (
     <div className="mt-16 w-full">
       <h3 className="text-center text-xl">Restaking Leaderboard</h3>
       <div className="flex flex-col lg:flex-row mt-3 w-full">
         <button
-          className={`table-button ${
-            activeButton === 0
-              ? "table-button-totals-active"
-              : "table-button-totals-inactive"
-          } py-3 px-4 lg:mr-2 grow border rounded focus:outline-none text-sm shadow-lg`}
-          onClick={() => {
-            handleToggleContent(leaderboardData.total, 0);
-            setCurrentPage(1);
-          }}
+          className={`table-button table-button-totals-${(activeTab === "total") ? "active" : "inactive"} py-3 px-4 lg:mr-2 grow border rounded focus:outline-none text-sm shadow-lg`}
+          onClick={() => setActiveTab("total")}
         >
           Total staked
         </button>
         {tokens.map((token, idx) => (
           <button
-            key={token}
-            className={`table-button ${
-              activeButton === (idx + 1)
-                ? `table-button-${token.toLowerCase()}-active`
-                : `table-button-${token.toLowerCase()}-inactive`
-            } py-3 px-4 lg:mr-2 grow border rounded focus:outline-none text-sm shadow-lg`}
-            onClick={() => {
-              handleToggleContent(
-                leaderboardData.partial[token]!,
-                idx + 1
-              );
-              setCurrentPage(1);
-            }}
+            key={idx}
+            className={`table-button table-button-${getTokenInfo(token).classId}-${(activeTab === token) ? "active" : "inactive"} py-3 px-4 lg:mr-2 grow border rounded focus:outline-none text-sm shadow-lg`}
+            onClick={() => setActiveTab(token)}
           >
-            {tokenLabels[idx]}
+            {getTokenInfo(token).label}
           </button>
         ))}
       </div>
@@ -91,9 +75,7 @@ export default function Leaderboard({ network }: LeaderboardProps) {
         <div className="leaderboard-table w-full mt-3 overflow-x-scroll">
           <table className="table w-full border-collapse">
             <thead
-              className={`text-base table-head table-head-${getActiveTabName(
-                activeButton
-              )}`}
+              className={`text-base table-head table-head-${(activeTab === "total") ? "total" : getTokenInfo(activeTab).classId}`}
             >
               <tr>
                 <th className="py-3 px-4 text-left">Rank</th>
@@ -103,33 +85,26 @@ export default function Leaderboard({ network }: LeaderboardProps) {
               </tr>
             </thead>
             <tbody>
-              {(activeData as LeaderboardStaker[])
-                .slice((currentPage - 1) * PAGE_SIZE, currentPage * PAGE_SIZE)
+              {activeData.slice(currentPage * PAGE_SIZE, (currentPage + 1) * PAGE_SIZE)
                 .map((userData, index) => (
                   <tr className="border-b-2" key={index}>
                     <td className="py-4 px-4 text-left text-md">
-                      {(currentPage - 1) * PAGE_SIZE + index + 1}
+                      {currentPage * PAGE_SIZE + index + 1}
                     </td>
-                    <td
-                      className="py-4 px-4 text-left text-sm font-normal w-full table-cell lg:hidden"
-                      style={{ cursor: "pointer" }}
-                      onClick={() => {
-                        window.open(getEtherscanAddressUrl(userData.depositor));
-                      }}
-                    >
-                      {userData.depositor?.endsWith(".eth")
-                        ? userData.depositor
-                        : getShortenedAddress(userData.depositor, 4, 6)}
-                    </td>
-                    <td
-                      className="py-4 px-4 text-left text-sm font-normal w-full hidden lg:table-cell"
-                      style={{ cursor: "pointer" }}
-                      onClick={() => {
-                        window.open(getEtherscanAddressUrl(userData.depositor));
-                      }}
-                    >
-                      {userData.depositor}
-                    </td>
+                    <Link href={getEtherscanAddressUrl(userData.depositor)} target="_blank">
+                      <td className="py-4 px-4 text-left text-sm font-normal w-full table-cell lg:hidden">
+                        {
+                          userData.depositor?.endsWith(".eth")
+                            ? userData.depositor
+                            : getShortenedAddress(userData.depositor, 4, 6)
+                        }
+                      </td>
+                    </Link>
+                    <Link href={getEtherscanAddressUrl(userData.depositor)} target="_blank">
+                      <td className="py-4 px-4 text-left text-sm font-normal w-full hidden lg:table-cell">
+                        {userData.depositor}
+                      </td>
+                    </Link>
                     <td className="py-4 px-4 text-right text-sm">
                       {userData.totalEth.toFixed(2)}
                     </td>
@@ -140,21 +115,21 @@ export default function Leaderboard({ network }: LeaderboardProps) {
 
           <div className=" pagination flex justify-center items-center mt-4 pb-6">
             <button
-              className={`${currentPage === 1 ? "disabled-arrow" : ""}`}
+              className={`${currentPage === 0 ? "disabled-arrow" : ""}`}
               onClick={() => setCurrentPage(currentPage - 1)}
-              disabled={currentPage === 1}
+              disabled={currentPage === 0}
             >
               <FontAwesomeIcon icon={faChevronLeft} />
             </button>
             <p className="mx-4">
-              Page {currentPage} of {totalPages}
+              Page {currentPage + 1} of {totalPages}
             </p>
             <button
               className={`${
                 currentPage === totalPages ? "disabled-arrow" : ""
               }`}
               onClick={() => setCurrentPage(currentPage + 1)}
-              disabled={currentPage === totalPages}
+              disabled={currentPage === totalPages - 1}
             >
               <FontAwesomeIcon icon={faChevronRight} />
             </button>
