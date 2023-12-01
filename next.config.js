92aabd0e3523fe776728a4b9c835da268605c80b
Adrian Tubal Páez Ruiz <adriantpaez@gmail.com>
2023-12-01 16:06:50 +0100

    feat: configure Next to build static site (#75)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true
+    },
     webpack: (config) => {
         config.externals.push({
             "utf-8-validate": "commonjs utf-8-validate",
