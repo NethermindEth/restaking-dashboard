a12572636b391c5520797e8d6018a9b52db56f4e
Franco Barpp Gomes <franco@nethermind.io>
2024-01-23 21:25:45 -0300

    refactor: Use next Script component for Matomo script

--- app/components/Matomo.tsx
+++ app/components/Matomo.tsx
@@ -1,36 +1,28 @@
 "use client"
 
-import React, { useEffect } from 'react';
+import React from "react";
+
+import Script from "next/script";
 
 const Matomo = () => {
-    useEffect(() => {
-        const loadScript = async () => {
-            const scriptContent = `
-                var _paq = window._paq = window._paq || [];
-                _paq.push(['disableCookies']);
-                _paq.push(['trackPageView']);
-                _paq.push(['enableLinkTracking']);
-                (function () {
-                    var u = "https://nethermind.matomo.cloud/";
-                    _paq.push(['setTrackerUrl', u + 'matomo.php']);
-                    _paq.push(['setSiteId', '4']);
-                    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-                    g.async = true; g.src = '//cdn.matomo.cloud/nethermind.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g, s);
-                })();`
-            const matomoScript = document.createElement('script');
-            matomoScript.innerHTML = scriptContent;
-
-            document.head.appendChild(matomoScript);
-
-            return () => {
-                document.head.removeChild(matomoScript);
-            };
-        };
-
-        loadScript();
-    }, []);
-
-    return null;
+  return (
+    <Script>
+      {`
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(["disableCookies"]);
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+          var u="https://nethermind.matomo.cloud/";
+          _paq.push(['setTrackerUrl', u+'matomo.php']);
+          _paq.push(['setSiteId', '4']);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.async=true; g.src='//cdn.matomo.cloud/nethermind.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+      `}
+    </Script>
+  );
 };
 
 export default Matomo;
