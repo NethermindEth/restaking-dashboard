7f2c6eed63d6acd5250ba6b9c0365b09a7b5f996
Franco Barpp Gomes <franco@nethermind.io>
2024-01-27 18:01:36 -0300

    refactor: Fetch more exchange rate data asynchronously

--- app/components/hooks/useShareRates.ts
+++ app/components/hooks/useShareRates.ts
@@ -24,42 +24,42 @@ export async function queryShareRates(network: SupportedNetwork, _: boolean = fa
   const rEth = getTokenNetworkInfo(network, "rEth")
     ? RocketTokenRETH__factory.connect(getTokenNetworkInfo(network, "rEth")!.address, provider)
     : null;
-  const rEthRate = rEth ? Number(await rEth.getExchangeRate()) / 1e18 : 0;
+  const rEthRate = rEth ? rEth.getExchangeRate().then(val => Number(val) / 1e18) : 0;
 
   const cbEth = getTokenNetworkInfo(network, "cbEth")
     ? StakedTokenV1__factory.connect(getTokenNetworkInfo(network, "cbEth")!.address, provider)
     : null;
-  const cbEthRate = cbEth ? Number(await cbEth.exchangeRate()) / 1e18 : 0;
+  const cbEthRate = cbEth ? cbEth.exchangeRate().then(val => Number(val) / 1e18) : 0;
 
   const wBEth = getTokenNetworkInfo(network, "wBEth")
     ? StakedTokenV1__factory.connect(getTokenNetworkInfo(network, "wBEth")!.address, provider)
     : null;
-  const wBEthRate = wBEth ? Number(await wBEth.exchangeRate()) / 1e18 : 0;
+  const wBEthRate = wBEth ? wBEth.exchangeRate().then(val => Number(val) / 1e18) : 0;
 
   const osEth = getTokenNetworkInfo(network, "osEth")
     ? PriceFeed__factory.connect(getTokenNetworkInfo(network, "osEth")!.feed!, provider)
     : null;
-  const osEthRate = osEth ? Number(await osEth.latestAnswer()) / 1e18 : 0;
+  const osEthRate = osEth ? osEth.latestAnswer().then(val => Number(val) / 1e18) : 0;
 
   const swEth = getTokenNetworkInfo(network, "swEth")
   ? SwETH__factory.connect(getTokenNetworkInfo(network, "swEth")!.address, provider)
   : null;
-  const swEthRate = swEth ? Number(await swEth.getRate()) / 1e18 : 0;
+  const swEthRate = swEth ? swEth.getRate().then(val => Number(val) / 1e18) : 0;
 
   const ankrEth = getTokenNetworkInfo(network, "ankrEth")
   ? AETH_R21__factory.connect(getTokenNetworkInfo(network, "ankrEth")!.address, provider)
   : null;
-  const ankrEthRate = ankrEth ? Number(await ankrEth.sharesToBonds(BigInt(1e18))) / 1e18 : 0;
+  const ankrEthRate = ankrEth ? ankrEth.sharesToBonds(BigInt(1e18)).then(val => Number(val) / 1e18) : 0;
 
   const ethX = getTokenNetworkInfo(network, "ethX")
   ? RocketTokenRETH__factory.connect(getTokenNetworkInfo(network, "ethX")!.feed!, provider)
   : null;
-  const ethXRate = ethX ? Number(await ethX.getExchangeRate()) / 1e18 : 0;
+  const ethXRate = ethX ? ethX.getExchangeRate().then(val => Number(val) / 1e18) : 0;
 
   const oEth = getTokenNetworkInfo(network, "oEth")
   ? OETH__factory.connect(getTokenNetworkInfo(network, "oEth")!.address, provider)
   : null;
-  const oEthRate = oEth ? Number(BigInt(1e36) / await oEth.rebasingCreditsPerToken()) / 1e18 : 0;
+  const oEthRate = oEth ? oEth.rebasingCreditsPerToken().then(val => Number(BigInt(1e36) / val) / 1e18) : 0;
 
   const strategyRates = supportedStrategyTokens.reduce((acc, token) => {
     const strategy = getTokenNetworkInfo(network, token)
@@ -78,14 +78,14 @@ export async function queryShareRates(network: SupportedNetwork, _: boolean = fa
 
   return {
     stEth: strategyRates["stEth"] ? await strategyRates["stEth"] : null,
-    rEth: strategyRates["rEth"] ? await strategyRates["rEth"] * rEthRate : null,
-    cbEth: strategyRates["cbEth"] ? await strategyRates["cbEth"] * cbEthRate : null,
-    wBEth: strategyRates["wBEth"] ? await strategyRates["wBEth"] * wBEthRate : null,
-    osEth: strategyRates["osEth"] ? await strategyRates["osEth"] * osEthRate : null,
-    swEth: strategyRates["swEth"] ? await strategyRates["swEth"] * swEthRate : null,
-    ankrEth: strategyRates["ankrEth"] ? await strategyRates["ankrEth"] * ankrEthRate : null,
-    ethX: strategyRates["ethX"] ? await strategyRates["ethX"] * ethXRate : null,
-    oEth: strategyRates["oEth"] ? await strategyRates["oEth"] * oEthRate : null,
+    rEth: strategyRates["rEth"] ? await strategyRates["rEth"] * await rEthRate : null,
+    cbEth: strategyRates["cbEth"] ? await strategyRates["cbEth"] * await cbEthRate : null,
+    wBEth: strategyRates["wBEth"] ? await strategyRates["wBEth"] * await wBEthRate : null,
+    osEth: strategyRates["osEth"] ? await strategyRates["osEth"] * await osEthRate : null,
+    swEth: strategyRates["swEth"] ? await strategyRates["swEth"] * await swEthRate : null,
+    ankrEth: strategyRates["ankrEth"] ? await strategyRates["ankrEth"] * await ankrEthRate : null,
+    ethX: strategyRates["ethX"] ? await strategyRates["ethX"] * await ethXRate : null,
+    oEth: strategyRates["oEth"] ? await strategyRates["oEth"] * await oEthRate : null,
     beacon: 1,
   };
 }
