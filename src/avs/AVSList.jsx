566f87ef1edecb39a74881e7c9fd7e417c29bd5e
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2024-06-19 18:32:15 +0530

    feat: add loading state for avs lists

--- src/avs/AVSList.jsx
+++ src/avs/AVSList.jsx
@@ -1,8 +1,9 @@
-import { reduceState } from '../shared/helpers';
+import { Skeleton } from '@nextui-org/react';
 import { useEffect } from 'react';
-import { useMutativeReducer } from 'use-mutative';
 import { useNavigate } from 'react-router-dom';
+import { useMutativeReducer } from 'use-mutative';
 import { useServices } from '../@services/ServiceContext';
+import { reduceState } from '../shared/helpers';
 import { useTailwindBreakpoint } from '../shared/useTailwindBreakpoint';
 import { formatNumber } from '../utils';
 
@@ -10,32 +11,21 @@ export default function AVSList({ onSelectionChange }) {
   const { avsService } = useServices();
   const compact = !useTailwindBreakpoint('md');
   const navigate = useNavigate();
-  const [state, dispatch] = useMutativeReducer(reduceState, {});
+  const [state, dispatch] = useMutativeReducer(reduceState, {
+    isFetchingAvsData: false
+  });
 
   useEffect(() => {
     async function fetchAVS() {
       try {
-        const data = await avsService.getAll();
+        dispatch({ isFetchingAvsData: true });
+        const response = await avsService.getAll();
+        const data = response.avs;
 
         for (let i = 0, count = data.length; i < count; i++) {
           let item = data[i];
-          item.tvl = 0;
+          item.tvl = item.strategiesTotal;
           item.address = item.address.toLowerCase();
-
-          const lowercaseStrategies = {};
-          for (let s in item.strategies) {
-            const strategy = item.strategies[s];
-            const shares = BigInt(strategy.shares);
-            const tokens = BigInt(strategy.tokens);
-            const totalValue = shares + tokens;
-
-            item.tvl += Number(totalValue);
-            lowercaseStrategies[s.toLowerCase()] = strategy;
-          }
-          item.strategies = lowercaseStrategies;
-
-          // Convert from wei to ether
-          item.tvl = item.tvl / 1e18;
         }
 
         // Sort descending by TVL
@@ -55,6 +45,8 @@ export default function AVSList({ onSelectionChange }) {
         dispatch({ avs: data });
       } catch {
         // TODO: handle error
+      } finally {
+        dispatch({ isFetchingAvsData: false });
       }
     }
 
@@ -74,39 +66,83 @@ export default function AVSList({ onSelectionChange }) {
       </div>
       <div className="bg-content1 border border-outline rounded-lg text-sm">
         <div className="flex flex-row gap-x-2 justify-between items-center p-4 text-foreground-1">
-          <div className="min-w-5"></div>
-          <div className="min-w-5"></div>
-          <span className="basis-full">Name</span>
+          <span className="basis-full md:pl-10">Name</span>
           <span className="basis-1/3">Stakers</span>
           <span className="basis-1/4">Operators</span>
           <span className="basis-1/3 text-end">TVL</span>
         </div>
-        {state.avs?.map((avs, i) => (
-          <div
-            key={`avs-item-${i}`}
-            onClick={() => handleAVSItemClick(avs)}
-            className={`border-t border-outline flex flex-row gap-x-2 justify-between items-center p-4 cursor-pointer hover:bg-default ${
-              state.selectedAVS === avs ? 'bg-content1' : ''
-            }`}
-          >
-            <div className="min-w-5">{i + 1}</div>
-            <img src={avs.metadata.logo} className="size-5 rounded-full" />
-            <span className="basis-full truncate">{avs?.metadata?.name}</span>
-            <span className="basis-1/3">
-              {formatNumber(avs.stakers, compact)}
-            </span>
-            <span className="basis-1/4">
-              {formatNumber(avs.operators, compact)}
-            </span>
-            <span className="basis-1/3 text-end">
-              <div>ETH {formatNumber(avs.tvl, compact)}</div>
-              <div className="text-foreground-1 text-xs">
-                USD {formatNumber(avs.tvl, compact)}
-              </div>
-            </span>
-          </div>
-        ))}
+        {state.isFetchingAvsData ? (
+          <AVSListSkeleton />
+        ) : (
+          state.avs?.map((avs, i) => (
+            <div
+              key={`avs-item-${i}`}
+              onClick={() => handleAVSItemClick(avs)}
+              className={`border-t border-outline flex flex-row gap-x-2 justify-between items-center p-4 cursor-pointer hover:bg-default ${
+                state.selectedAVS === avs ? 'bg-content1' : ''
+              }`}
+            >
+              <div className="min-w-5">{i + 1}</div>
+              <img src={avs.metadata.logo} className="size-5 rounded-full" />
+              <span className="md:basis-full md:w-full w-[180px] truncate">
+                {avs?.metadata?.name}
+              </span>
+              <span className="basis-1/3">
+                {formatNumber(avs.stakers, compact)}
+              </span>
+              <span className="basis-1/4">
+                {formatNumber(avs.operators, compact)}
+              </span>
+              <span className="basis-1/3 text-end">
+                <div>ETH {formatNumber(avs.tvl, compact)}</div>
+                <div className="text-foreground-1 text-xs">
+                  USD {formatNumber(avs.tvl, compact)}
+                </div>
+              </span>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
 }
+
+const AVSListSkeleton = () => {
+  return (
+    <div>
+      {[...Array(10)].map((item, i) => (
+        <div
+          key={i}
+          className="p-4 flex justify-normal gap-4 md:gap-8 text-foreground-1 border-t border-outline w-full"
+        >
+          <div className="md:w-10/12 w-6/12">
+            <Skeleton
+              style={{ background: '#191C2C' }}
+              className="h-8 rounded-md w-4/5 md:w-2/3"
+            />
+          </div>
+          <div className="pl-5 flex justify-between gap-5 w-10/12">
+            <div className="w-3/12">
+              <Skeleton
+                style={{ background: '#191C2C' }}
+                className="h-8 rounded-md w-full "
+              />
+            </div>
+            <div className="w-3/12">
+              <Skeleton
+                style={{ background: '#191C2C' }}
+                className="h-8 rounded-md w-full "
+              />
+            </div>
+            <div className="w-3/12">
+              <Skeleton
+                style={{ background: '#191C2C' }}
+                className="h-8 rounded-md w-full "
+              />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
