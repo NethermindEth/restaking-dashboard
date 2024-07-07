693cffec747d25885ee98d38ee212cd46d8c297e
Hong Zheng You <107991825+vincenthongzy@users.noreply.github.com>
2024-07-07 12:26:12 +0800

    Add LST page (#107)

--- src/@services/EigenlayerService.js
+++ src/@services/EigenlayerService.js
@@ -1,8 +1,19 @@
 import { apiGet } from './apiCall';
 
 export default class EigenlayerService {
-  async getEigenlayerTVLOvertime() {
+  async getEigenLayerTVLOvertime() {
     const response = await apiGet(`/eigenlayer/tvl`);
+
+    if (response.ok) {
+      return await response.json();
+    }
+
+    // TODO: Handle error
+    return await response.json();
+  }
+
+  async getEigenLayerLSTTotalValue() {
+    const response = await apiGet(`/eigenlayer/tvl/lst`);
 
     if (response.ok) {
       return await response.json();
