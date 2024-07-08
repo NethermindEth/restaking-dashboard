f3f2309304a02242471af178f68098b465106147
Zishan Mohd <zishan2539@gmail.com>
2024-07-08 22:44:51 +0530

    fix padding

--- src/lst/LST.jsx
+++ src/lst/LST.jsx
@@ -72,19 +72,18 @@ export default function LST() {
 
   return (
     <div>
-      <div className="font-display font-medium m-4 text-foreground-1 text-3xl">
-        Liquidity Staked Tokens
+      <div className="font-display font-medium my-4 text-foreground-1 text-3xl">
+        Liquid Staking Tokens
       </div>
-      <div className="font-display font-medium px-4 mb-4 text-foreground-1 text-xs">
-        Liquidity Staking Tokens restaked on EigenLayer Liquidity staking tokens
-        (LSTs) are a form of cryptocurrency that represent staked assets in a
-        liquidity pool or staking protocol. They allow users to earn rewards
-        from staking while maintaining liquidity, as the tokens can be traded,
-        transferred, or used in various DeFi applications. LSTs can be restaked
-        on EigenLayer to earn restaking rewards, further maximizing yield for
-        restakers.
+      <div className="font-display font-medium mb-4 text-foreground-1 text-xs">
+        Liquid Staking Tokens (LSTs) restaked on EigenLayer are a form of
+        cryptocurrency that represent staked assets in a liquidity pool or
+        staking protocol. They allow users to earn rewards from staking while
+        maintaining liquidity, as the tokens can be traded, transferred, or used
+        in various DeFi applications. LSTs can be restaked on EigenLayer to earn
+        restaking rewards, further maximizing yield for restaker
       </div>
-      <div className="bg-content1 border border-outline rounded-lg text-sm m-4 mt-6">
+      <div className="bg-content1 border border-outline rounded-lg text-sm my-4 mt-6">
         <div className="flex flex-row gap-x-2 justify-between items-center p-4 text-foreground-1">
           <div className="flex flex-col items-center basis-1/2 text-center">
             <span>TVL</span>
