c7efd019fd80947521b20b1130bbf541f16de938
Zishan Mohd <zishan2539@gmail.com>
2024-07-18 13:24:50 +0530

    Update pagination to use NextUI components

--- src/shared/Pagination.jsx
+++ src/shared/Pagination.jsx
@@ -1,4 +1,4 @@
-import { PaginationItemType, usePagination } from '@nextui-org/react';
+import { Button, Pagination as NextUIPagination } from '@nextui-org/react';
 
 const Pagination = ({
   totalPages,
@@ -7,50 +7,36 @@ const Pagination = ({
   handlePrevious,
   handlePageClick
 }) => {
-  const { activePage, range } = usePagination({
-    total: totalPages,
-    showControls: true,
-    page: currentPage
-  });
-
   return (
-    <div className="flex border-t border-outline gap-x-2 justify-between items-center p-4 text-foreground-1 mt-austo">
-      <button
-        className={`material-symbols-outlined ${activePage === 1 && 'text-disabled'} disabled:cursor-not-allowed`}
-        onClick={handlePrevious}
-        disabled={currentPage === 1}
+    <div className="flex border-t border-outline justify-between items-center p-4">
+      <Button
+        className={`bg-transparent ${currentPage === 1 && 'text-disabled'}`}
+        onPress={handlePrevious}
+        isDisabled={currentPage === 1}
+        isIconOnly
       >
-        arrow_back_ios
-      </button>
-      <div className="flex gap-x-2">
-        {range
-          .filter(
-            r => r !== PaginationItemType.PREV && r !== PaginationItemType.NEXT
-          )
-          .map((page, i) => {
-            if (page === PaginationItemType.DOTS) {
-              return <span key={`dot-${i}`}>...</span>;
-            }
+        <span className="material-symbols-outlined">arrow_back_ios</span>
+      </Button>
 
-            return (
-              <button
-                onClick={() => handlePageClick(page)}
-                key={`page-${page}`}
-                aria-label={`page ${page}`}
-                className={`${activePage == page ? 'text-foreground-1' : 'text-slate-500'}`}
-              >
-                {page}
-              </button>
-            );
-          })}
-      </div>
-      <button
-        className={`material-symbols-outlined ${activePage === totalPages && 'text-disabled'} disabled:cursor-not-allowed`}
-        onClick={handleNext}
-        disabled={currentPage === totalPages}
+      <NextUIPagination
+        classNames={{
+          item: 'bg-transparent [&[data-hover=true]:not([data-active=true])]:bg-default',
+          wrapper: 'gap-1'
+        }}
+        total={totalPages}
+        color="default"
+        page={currentPage}
+        onChange={handlePageClick}
+      />
+
+      <Button
+        className={`bg-transparent ${currentPage === totalPages && 'text-disabled'}`}
+        onPress={handleNext}
+        isDisabled={currentPage === totalPages}
+        isIconOnly
       >
-        arrow_forward_ios
-      </button>
+        <span className="material-symbols-outlined">arrow_forward_ios</span>
+      </Button>
     </div>
   );
 };
