835397a7a315445f66452deb301ce267d1d06b5a
Ruben Buniatyan <rubo@users.noreply.github.com>
2024-07-24 19:36:08 +0200

    Revise tabs background color

--- src/shared/slots.js
+++ src/shared/slots.js
@@ -1,4 +1,4 @@
 export const tabs = Object.freeze({
   cursor: 'border border-outline shadow-none rounded-md',
-  tabList: 'bg-background border border-outline rounded-lg'
+  tabList: 'bg-transparent border border-outline rounded-lg'
 });
