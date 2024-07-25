8d7e2b85316537c8c34c5ffef0616ab00ef9bece
Zishan Mohd <zishan2539@gmail.com>
2024-07-25 21:34:01 +0530

    refactor operator list

--- src/operators/OperatorsList.jsx
+++ src/operators/OperatorsList.jsx
@@ -1,65 +1,109 @@
-import { useCallback, useEffect } from 'react';
-import { useServices } from '../@services/ServiceContext';
-import { useMutativeReducer } from 'use-mutative';
-import { reduceState } from '../shared/helpers';
-import { Link, useSearchParams } from 'react-router-dom';
+import { formatETH, formatNumber, formatUSD } from '../shared/formatters';
+import { handleServiceError, reduceState } from '../shared/helpers';
+import {
+  Input,
+  Skeleton,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow
+} from '@nextui-org/react';
+import { useCallback, useEffect, useRef } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import ErrorMessage from '../shared/ErrorMessage';
 import ListPagination from '../shared/ListPagination';
-import { Input, Skeleton } from '@nextui-org/react';
-import { formatNumber } from '../utils';
-import { useTailwindBreakpoint } from '../shared/useTailwindBreakpoint';
+import ThirdPartyLogo from '../shared/ThirdPartyLogo';
 import useDebouncedSearch from '../shared/hooks/useDebouncedSearch';
+import { useMutativeReducer } from 'use-mutative';
+import { useServices } from '../@services/ServiceContext';
+
+const columns = [
+  {
+    key: 'operator',
+    label: 'Operators',
+    className: 'w-64 md:w-2/5 ps-12'
+  },
+  {
+    key: 'restaker',
+    label: 'Restakers',
+    className: 'text-end w-36 md:w-1/5'
+  },
+
+  {
+    key: 'tvl',
+    label: 'TVL',
+    className: 'text-end w-40 md:w-2/5'
+  }
+];
 
 const OperatorsList = () => {
   const { operatorService } = useServices();
   const [searchParams, setSearchParams] = useSearchParams();
-  const compact = !useTailwindBreakpoint('sm');
+  const navigate = useNavigate();
+  const abortController = useRef(null);
   const [state, dispatch] = useMutativeReducer(reduceState, {
+    operators: [],
+    isFetchingData: false,
     searchTerm: searchParams.get('search'),
-    searchTriggered: false,
     error: null,
-    isFetchingOperatorData: true
+    rate: 1,
+    searchTriggered: false,
+    sortDescriptor: searchParams.get('sort')
+      ? {
+          column: searchParams.get('sort').replace('-', ''),
+          direction: searchParams.get('sort').startsWith('-')
+            ? 'descending'
+            : 'ascending'
+        }
+      : { column: 'tvl', direction: 'descending' }
   });
-  const debouncedSearchTerm = useDebouncedSearch(state.searchTerm, 300);
 
-  const getOperators = async (pageIndex, search) => {
-    try {
-      dispatch({
-        isFetchingOperatorData: true
-      });
-      const data = await operatorService.getAll(pageIndex - 1, search);
-      dispatch({
-        operators: data.results,
-        isFetchingOperatorData: false,
-        totalPages: Math.ceil(data.totalCount / 10),
-        rate: 1
-      });
-    } catch {
-      dispatch({
-        error: 'Failed to fetch Operator data',
-        isFetchingOperatorData: false
-      });
-    }
-  };
+  const debouncedSearchTerm = useDebouncedSearch(state.searchTerm ?? '', 300);
 
-  const handleNext = useCallback(() => {
-    const currentPage = parseInt(searchParams.get('page'));
-    if (currentPage + 1 <= state.totalPages) {
-      setSearchParams({ page: currentPage + 1 });
-    }
-  }, [searchParams, state.totalPages, setSearchParams, getOperators]);
+  const fetchOperators = useCallback(
+    async (pageNumber, search, sort) => {
+      try {
+        dispatch({ isFetchingData: true, error: null });
 
-  const handlePrevious = useCallback(() => {
-    const currentPage = parseInt(searchParams.get('page'));
-    if (currentPage - 1 >= 1) {
-      setSearchParams({ page: currentPage - 1 });
-    }
-  }, [searchParams, setSearchParams, getOperators]);
+        if (abortController.current) {
+          abortController.current.abort();
+        }
+        abortController.current = new AbortController();
+
+        const response = await operatorService.getAll(
+          pageNumber,
+          search,
+          sort,
+          abortController.current.signal
+        );
+
+        dispatch({
+          operators: response.results,
+          isFetchingData: false,
+          rate: response.rate,
+          totalPages: Math.ceil(response.totalCount / 10)
+        });
+
+        abortController.current = null;
+      } catch (e) {
+        if (e.name !== 'AbortError') {
+          dispatch({
+            error: handleServiceError(e),
+            isFetchingData: false
+          });
+        }
+      }
+    },
+    [operatorService, dispatch]
+  );
 
   const handlePageClick = useCallback(
     page => {
       setSearchParams({ page });
     },
-    [setSearchParams, getOperators]
+    [setSearchParams]
   );
 
   const handleSearch = e => {
@@ -67,132 +111,191 @@ const OperatorsList = () => {
   };
 
   useEffect(() => {
-    const page = searchParams.get('page');
+    const page = searchParams.get('page') ?? 1;
 
     const params = {};
-    if (page && debouncedSearchTerm) {
-      params.page = state.searchTriggered ? 1 : page; // If user has searched something update the page number to 1
-      params.search = debouncedSearchTerm;
-    } else if (page) {
-      params.page = page;
-    } else if (debouncedSearchTerm) {
-      params.page = 1;
-      params.search = debouncedSearchTerm;
-    } else {
-      params.page = 1;
+    params.page = state.searchTriggered ? 1 : page; // If user has searched something update the page number to 1
+    if (debouncedSearchTerm) params.search = debouncedSearchTerm;
+    if (state.sortDescriptor) {
+      params.sort =
+        state.sortDescriptor.direction === 'ascending'
+          ? state.sortDescriptor.column
+          : `-${state.sortDescriptor.column}`;
     }
+
     setSearchParams(params, { replace: true });
-    getOperators(params.page, params.search);
+    fetchOperators(params.page, params.search, params.sort);
     dispatch({ searchTriggered: false });
-  }, [searchParams, debouncedSearchTerm]);
+  }, [
+    debouncedSearchTerm,
+    dispatch,
+    fetchOperators,
+    searchParams,
+    setSearchParams,
+    state.searchTriggered,
+    state.sortDescriptor
+  ]);
 
   useEffect(() => {
-    dispatch({ searchTriggered: true });
-  }, [debouncedSearchTerm]);
+    if (debouncedSearchTerm) dispatch({ searchTriggered: true });
+  }, [dispatch, debouncedSearchTerm]);
 
   return (
-    <div>
-      <div className="mb-1 font-display text-3xl font-medium text-foreground-1">
+    <div className="flex h-full flex-col">
+      <div className="font-display text-3xl font-medium text-foreground-1">
         Operators
       </div>
-      <div className="mb-6 flex w-full flex-col items-center justify-between gap-4 lg:flex-row">
-        <span className="text-foreground-2">
+
+      <div className="mb-4 mt-3 flex w-full flex-col items-start justify-between gap-4 lg:flex-row lg:items-center">
+        <div className="text-foreground-1 lg:w-2/3">
           Operators run AVS software built on top of EigenLayer. Operators
           register in EigenLayer and allow restakers to delegate to them, then
           opt in to secure various services (AVSs) built on top of EigenLayer.
           Operators may also be Stakers; these are not mutually exclusive.
-        </span>
+        </div>
         <Input
-          value={state.searchTerm}
+          classNames={{
+            inputWrapper:
+              'border-outline data-[hover=true]:border-foreground-1',
+            input: 'placeholder:text-subtitle'
+          }}
+          value={state.searchTerm ?? ''}
           onChange={handleSearch}
           type="text"
-          placeholder="Search by operator"
+          color="primary"
+          placeholder="Search by Operator"
           radius="sm"
           className="lg:w-96"
           variant="bordered"
-          endContent={<span className="material-symbols-outlined">search</span>}
+          endContent={
+            <span className="material-symbols-outlined text-foreground-2">
+              search
+            </span>
+          }
         />
       </div>
-      <div className="rounded-lg border border-outline bg-content1 text-sm">
-        <div className="flex flex-row items-center justify-between gap-x-2 p-4 text-foreground-1">
-          <div className="min-w-5 lg:min-w-8"></div>
-          <span className="basis-1/2">Operators</span>
-          <span className="basis-1/4">Restakers</span>
-          <span className="basis-1/3 text-end">TVL</span>
-        </div>
-        {state.isFetchingOperatorData ? (
-          <OperatorListSkeleton />
-        ) : (
-          <>
-            {state.operators?.map((op, i) => (
-              <Link
-                to={`/operators/${op.address}`}
-                key={`operator-item-${i}`}
-                className={`flex cursor-pointer flex-row items-center justify-between gap-x-2 border-t border-outline px-4 py-2 hover:bg-default`}
-              >
-                <div className="min-w-5 text-foreground-2 lg:min-w-8">
-                  {(searchParams.get('page') - 1) * 10 + i + 1}
-                </div>
-                {op.metadata?.logo ? (
-                  <img
-                    className="h-5 min-w-5 rounded-full"
-                    src={op.metadata?.logo}
-                  />
-                ) : (
-                  <span className="material-symbols-outlined flex h-5 min-w-5 items-center justify-center rounded-full text-lg text-yellow-300">
-                    warning
-                  </span>
-                )}
-                <span className="basis-1/2 truncate">
-                  {op.metadata?.name ?? 'Unknown'}
-                </span>
-                <span className="basis-1/4">
-                  {formatNumber(op.stakerCount, compact)}
-                </span>
-                <div className="basis-1/3 text-end">
-                  <div>
-                    ${formatNumber(op.strategiesTotal * state.rate, compact)}
-                  </div>
-                  <div className="text-xs text-subtitle">
-                    {formatNumber(op.strategiesTotal, compact)} ETH
-                  </div>
-                </div>
-              </Link>
-            ))}
 
+      {!state.isFetchingData && state.error && (
+        <div className="flex flex-1 flex-col items-center justify-center rounded-lg border border-outline bg-content1 text-sm">
+          <ErrorMessage error={state.error} />
+        </div>
+      )}
+
+      {!state.error && (
+        <div className="flex flex-1 flex-col rounded-lg border border-outline bg-content1 text-sm">
+          <Table
+            aria-label="Operator list"
+            hideHeader={!state.isFetchingData && state.operators.length == 0}
+            layout="fixed"
+            removeWrapper
+            classNames={{
+              base: 'h-full overflow-x-auto',
+              table: 'h-full',
+              thead: '[&>tr:last-child]:hidden'
+            }}
+            sortDescriptor={state.sortDescriptor}
+            onSortChange={e => dispatch({ sortDescriptor: e })}
+          >
+            <TableHeader columns={columns}>
+              {column => (
+                <TableColumn
+                  allowsSorting
+                  className={`text-foreground-active bg-transparent py-4 text-sm font-normal leading-5 data-[hover=true]:text-foreground-2 ${column.className}`}
+                  key={column.key}
+                >
+                  {column.label}
+                </TableColumn>
+              )}
+            </TableHeader>
+            <TableBody
+              emptyContent={
+                <div className="flex flex-col items-center justify-center">
+                  <span className="text-lg text-foreground-2">
+                    No Operators found for &quot;
+                    {debouncedSearchTerm.length > 42
+                      ? `${debouncedSearchTerm.substring(0, 42)}...`
+                      : debouncedSearchTerm}
+                    &quot;
+                  </span>
+                </div>
+              }
+            >
+              {state.isFetchingData
+                ? [...Array(10)].map((_, i) => (
+                    <TableRow key={i} className="border-t border-outline">
+                      <TableCell className="w-2/5 py-6 pe-8 ps-4">
+                        <Skeleton className="h-4 rounded-md bg-default" />
+                      </TableCell>
+                      <TableCell className="w-1/5 py-6 pe-8 ps-4">
+                        <Skeleton className="h-4 rounded-md bg-default" />
+                      </TableCell>
+
+                      <TableCell className="w-2/5 py-6 pe-8 ps-4">
+                        <Skeleton className="h-4 rounded-md bg-default" />
+                      </TableCell>
+                    </TableRow>
+                  ))
+                : state.operators?.map((operator, i) => (
+                    <TableRow
+                      onClick={() =>
+                        navigate(`/operators/${operator.address}`, {
+                          state: { operator }
+                        })
+                      }
+                      key={`operator-item-${i}`}
+                      className="cursor-pointer border-t border-outline transition-colors hover:bg-default"
+                    >
+                      <TableCell className="p-4">
+                        <div className="flex items-center gap-x-3">
+                          <span className="min-w-5">
+                            {(searchParams.get('page') - 1) * 10 + i + 1}
+                          </span>
+                          <ThirdPartyLogo
+                            className="size-8 min-w-8"
+                            url={operator.metadata?.logo}
+                          />
+                          <span className="truncate">
+                            {operator.metadata?.name || operator.address}
+                          </span>
+                        </div>
+                      </TableCell>
+                      <TableCell className="pe-8 text-end">
+                        {formatNumber(operator.stakerCount)}
+                      </TableCell>
+
+                      <TableCell className="pe-8 text-end">
+                        <div>
+                          {formatUSD(operator.strategiesTotal * state.rate)}
+                        </div>
+                        <div className="text-xs text-subtitle">
+                          {formatETH(operator.strategiesTotal)}
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+
+              {!state.isFetchingData &&
+                state.operators?.length > 0 &&
+                [...Array(10 - state.operators.length)].map((_, i) => (
+                  <TableRow key={i}>
+                    <TableCell className="h-full w-2/5"></TableCell>
+                    <TableCell className="w-1/5"></TableCell>
+                    <TableCell className="w-2/5"></TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+          {state.operators && state.operators.length !== 0 && (
             <ListPagination
-              total={state.totalPages}
-              page={parseInt(searchParams.get('page'))}
               onChange={handlePageClick}
+              page={parseInt(searchParams.get('page') || '1')}
+              total={state.totalPages}
             />
-          </>
-        )}
-      </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
 
 export default OperatorsList;
-
-const OperatorListSkeleton = () => {
-  return (
-    <div>
-      {[...Array(10)].map((item, i) => (
-        <div
-          key={i}
-          className="flex w-full justify-normal gap-4 border-t border-outline p-4 text-foreground-1 md:gap-8"
-        >
-          <div className="basis-1/2">
-            <Skeleton className="h-6 w-4/5 rounded-md dark:bg-default md:w-2/3" />
-          </div>
-          <div className="basis-1/4">
-            <Skeleton className="h-6 w-4/5 rounded-md dark:bg-default md:w-2/3" />
-          </div>{' '}
-          <div className="basis-1/3">
-            <Skeleton className="h-6 w-full rounded-md dark:bg-default" />
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
