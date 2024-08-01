3ac0bf8f32a5956536a586be088818ae13b82715
Ruben Buniatyan <rubo@users.noreply.github.com>
2024-08-02 00:03:46 +0200

    Update LRT logos

--- src/lrt/helpers.js
+++ src/lrt/helpers.js
@@ -3,25 +3,55 @@ export const protocols = {
     index: 0,
     name: 'Bedrock',
     symbol: 'uniETH',
-    logo: '/bedrock.jpg'
+    logo: '/images/bedrock.png'
   },
   eigenpie: {
     index: 6,
     name: 'Eigenpie',
     symbol: 'egETH m*ETH',
-    logo: '/eigenpie.jpg'
+    logo: '/images/eigenpie.png'
   },
-  etherfi: { index: 1, name: 'ether.fi', symbol: 'eETH', logo: '/etherfi.jpg' },
-  euclid: { index: 9, name: 'Euclid', symbol: 'elETH', logo: '/euclid.jpg' },
+  etherfi: {
+    index: 1,
+    name: 'ether.fi',
+    symbol: 'eETH',
+    logo: '/images/etherfi.svg'
+  },
+  euclid: {
+    index: 9,
+    name: 'Euclid',
+    symbol: 'elETH',
+    logo: '/images/euclid.svg'
+  },
   inception: {
     index: 4,
     name: 'Inception',
     symbol: 'in*ETH',
-    logo: '/inception.png'
+    logo: '/images/inception.png'
   },
-  kelp: { index: 3, name: 'Kelp', symbol: 'rsETH', logo: '/kelp.jpg' },
-  prime: { index: 8, name: 'Prime', symbol: 'primeETH', logo: '/prime.png' },
-  puffer: { index: 7, name: 'Puffer', symbol: 'pufETH', logo: '/puffer.jpg' },
-  renzo: { index: 2, name: 'Renzo', symbol: 'ezETH', logo: '/renzo.svg' },
-  swell: { index: 5, name: 'Swell', symbol: 'rswETH', logo: '/swell.jpg' }
+  kelp: { index: 3, name: 'Kelp', symbol: 'rsETH', logo: '/images/kelp.png' },
+  prime: {
+    index: 8,
+    name: 'Prime',
+    symbol: 'primeETH',
+    logo: '/images/prime.svg'
+  },
+  puffer: {
+    index: 7,
+    name: 'Puffer',
+    symbol: 'pufETH',
+    logo: '/images/puffer.svg'
+  },
+  renzo: {
+    index: 2,
+    name: 'Renzo',
+    symbol: 'ezETH',
+    logo: '/images/renzo.png'
+  },
+  swell: {
+    index: 5,
+    name: 'Swell',
+    symbol: 'rswETH',
+    logo: '/images/swell.svg'
+  }
 };
