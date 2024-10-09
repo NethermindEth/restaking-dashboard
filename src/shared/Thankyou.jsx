5932d2bfecbe2181ffd1ea0daabb32c3dc1d197a
Satyam Bansal <satyamsgsits1994@gmail.com>
2024-10-09 16:37:00 +0530

    chore: Fix prettier errors

--- src/shared/Thankyou.jsx
+++ src/shared/Thankyou.jsx
@@ -1,26 +1,27 @@
-import { useNavigate } from "react-router-dom";
-
+import { useNavigate } from 'react-router-dom';
 
 export default function Thankyou() {
   const navigate = useNavigate();
 
   return (
-    <div className="py-20 text-default-700 text-center max-w-2xl mx-auto">
-      <h1 className="text-3xl font-medium mb-2">
-        Thanks for your support!
-      </h1>
-      <h2 className="text-xl mb-3">
-        You are now subscribed
-      </h2>
-      <p className="text-default-2 mb-6">
-        Thank you for supporting our work and helping us bring more transparency and insights to the restaking ecosystem.
+    <div className="mx-auto max-w-2xl py-20 text-center text-default-700">
+      <h1 className="mb-2 text-3xl font-medium">Thanks for your support!</h1>
+      <h2 className="mb-3 text-xl">You are now subscribed</h2>
+      <p className="mb-6 text-default-2">
+        Thank you for supporting our work and helping us bring more transparency
+        and insights to the restaking ecosystem.
       </p>
-      <div className="w-12 h-12 rounded-full bg-success text-background mx-auto mb-12">
-        <span class="material-symbols-outlined text-4xl leading-[48px] font-semibold">
+      <div className="mx-auto mb-12 h-12 w-12 rounded-full bg-success text-background">
+        <span className="material-symbols-outlined text-4xl font-semibold leading-[48px]">
           check
         </span>
       </div>
-      <button className="text-sm text-default-2 underline" onClick={() => { navigate('/subscriptions') }}>
+      <button
+        className="text-sm text-default-2 underline"
+        onClick={() => {
+          navigate('/subscriptions');
+        }}
+      >
         Take me to the subscription page
       </button>
     </div>
